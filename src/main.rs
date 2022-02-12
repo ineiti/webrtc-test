fa125d7c746af5fac3ed2180a4307e2d2beb2526
Linus Gasser <ineiti@gasser.blue>
2022-02-12 21:43:57 +0100

    Using flavor=multi-thread in test

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ use webrtc_test::make_offer;
 
 #[tokio::main]
 async fn main() -> Result<(), webrtc::Error> {
+    env_logger::init();
+    make_offer().await
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_make_offer() -> Result<(), webrtc::Error> {
+    env_logger::init();
     make_offer().await
 }
--- tests/offer.rs
+++
@@ -1,6 +0,0 @@
-use webrtc_test::make_offer;
-
-#[tokio::test]
-async fn test_make_offer() -> Result<(), webrtc::Error> {
-    make_offer().await
-}
